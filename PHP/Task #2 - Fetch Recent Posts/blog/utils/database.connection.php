03bd44bf0fdd98a7b84f460e29d906a2da6e9498
omarabdelaz1z <omarabdelaziz042@gmail.com>
2021-09-14 02:58:44 +0200

    MySQL Configuration

---
+++ PHP/Task #2 - Fetch Recent Posts/blog/utils/database.connection.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'database.config.php';
+
+
+function connect(){
+    try{
+        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+        $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+        return $connection;
+    }
+    catch(mysqli_sql_exception $e){
+        throw $e;
+    }
+}

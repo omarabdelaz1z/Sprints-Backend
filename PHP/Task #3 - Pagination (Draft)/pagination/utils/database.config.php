ccb99a67e09da06ed8f0b747df044ba331ee487a
omarabdelaz1z <omarabdelaziz042@gmail.com>
2021-09-16 18:32:24 +0200

    changed to the old database: blog instead of blog2

--- PHP/Task #3 - Pagination (Draft)/pagination/utils/database.config.php
+++ PHP/Task #3 - Pagination (Draft)/pagination/utils/database.config.php
@@ -3,4 +3,4 @@
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'blog2');
+define('DB_NAME', 'blog');
--- PHP/Task #3 - Pagination (Draft)/pagination/utils/database.connection.php
+++ PHP/Task #3 - Pagination (Draft)/pagination/utils/database.connection.php
@@ -6,6 +6,7 @@ function connect(){
     try{
         mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
         $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+        
         return $connection;
     }
     catch(mysqli_sql_exception $e){

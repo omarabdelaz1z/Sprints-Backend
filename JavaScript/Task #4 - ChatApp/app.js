da01edbf254539450f39443bac4cd79128d02ada
omarabdelaz1z <omarabdelaziz042@gmail.com>
2021-09-19 01:03:49 +0200

    Express Server

---
+++ JavaScript/Task #4 - ChatApp/app.js
@@ -0,0 +1,87 @@
+"use strict";
+require("dotenv").config();
+const express = require("express");
+const Pusher = require("pusher");
+
+const channels = {};
+const PORT = process.env.PORT || 3000;
+const generateID = () => Math.random().toString(36).substr(2, 9);
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const pusher = new Pusher({
+  appId: process.env.APP_ID,
+  key: process.env.KEY,
+  secret: process.env.SECRET,
+  cluster: process.env.CLUSTER,
+  useTLS: true,
+});
+
+app.post("/message", async (req, res) => {
+  const { group } = req.body;
+  console.log(group);
+  console.log(channels);
+
+  // Trigger a specific a channel that a message with sent to a specifc group.
+  const receivedDate = new Date().toLocaleString(undefined, {
+    timeStyle: "short",
+    dateStyle: "medium",
+  });
+
+  try {
+    await pusher.trigger(group, "message", {
+      ...req.body,
+      receivedDate,
+    });
+
+    // Once a message sent, return subscription count as response.
+    res.status(200).send();
+  } catch (error) {
+    res.status(503).send({ errorMessage: error });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { group, username } = req.body;
+
+  const userId = generateID();
+  const user = { userId, username, group };
+
+  // Check if the channel exists:
+  // Add the group and assign the user to list of users of that group.
+  // Otherwise, the channel exists and the user is added to the list.
+  try {
+    if (!channels[group]) channels[group] = [user];
+    else channels[group].push(user);
+
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(401).redirect("/");
+  }
+});
+
+app.post("/logout", async (req, res) => {
+  const { userId, group } = req.body;
+  try {
+    channels[group] = channels[group].filter((user) => user.userId !== userId);
+    res.status(200).end();
+  } catch (error) {
+    res.status(404).send({ errorMessage: "Group not found" });
+  }
+});
+
+app.get("/usercount/:group/", (req, res) => {
+  const { group } = req.params;
+  const channel = channels?.[group];
+
+  if (!channel) res.status(404).json({ message: "Group not found" });
+  else res.status(200).json({ group, count: channel.length });
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
